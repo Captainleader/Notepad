9fa18fa30b0efc95993e54115641ff1b3b9aedcd
Captainleader <157316418+Captainleader@users.noreply.github.com>
2025-07-30 23:54:20 +0100

    Update sdl_addon.hpp

--- sdl_addon.hpp
+++ sdl_addon.hpp
@@ -1,75 +1,8 @@
 #pragma once
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
-#include <windows.h>
-#include <commdlg.h> 
-#include <string>
-#include <iostream>
 
-    
-// sdl button
-class Button {
-public:
-    int x, y, w, h;
-    std::string title;
-    bool clicked = false;
-
-    SDL_Color gray = {34, 53, 66, 255};
-    SDL_Color blue = {37, 160, 245, 255};
-    SDL_Color white = {221, 227, 237, 255};
-
-private:
-    Uint32 clickTime = 0; // When was clicked
-    const Uint32 clickDuration = 300; // ms to keep blue color
-
-public:
-    Button(int x_, int y_, int w_, int h_, const std::string& title_)
-        : x(x_), y(y_), w(w_), h(h_), title(title_) {}
-
-    void handleEvent(const SDL_Event& e) {
-        if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT) {
-            int mx = e.button.x;
-            int my = e.button.y;
-            if (mx >= x && mx <= x + w && my >= y && my <= y + h) {
-                clicked = true;
-                clickTime = SDL_GetTicks();
-            }
-        }
-    }
-
-    void update() {
-        if (clicked) {
-            Uint32 now = SDL_GetTicks();
-            if (now - clickTime > clickDuration) {
-                clicked = false;
-            }
-        }
-    }
-
-    void draw(SDL_Renderer* ren, TTF_Font* font) {
-        SDL_Color bgColor = clicked ? blue : gray;
-
-        // Draw button rectangle
-        SDL_SetRenderDrawColor(ren, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
-        SDL_Rect rect = {x, y, w, h};
-        SDL_RenderFillRect(ren, &rect);
-
-        // Render title text
-        SDL_Surface* textSurface = TTF_RenderUTF8_Blended(font, title.c_str(), white);
-        if (textSurface) {
-            SDL_Texture* textTexture = SDL_CreateTextureFromSurface(ren, textSurface);
-            int textW = textSurface->w;
-            int textH = textSurface->h;
-            SDL_FreeSurface(textSurface);
-
-            SDL_Rect dst = {x + (w - textW) / 2, y + (h - textH) / 2, textW, textH};
-            SDL_RenderCopy(ren, textTexture, nullptr, &dst);
-            SDL_DestroyTexture(textTexture);
-        }
-    }
-};
-
-
+   
 // open a file
 
 struct FileDialogResult {
@@ -97,3 +30,6 @@ inline FileDialogResult OpenFileDialog(HWND owner = nullptr) {
         return { false, L"" };
     }
 }
+
+
+
